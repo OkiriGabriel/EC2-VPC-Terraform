830eefe224458741594271136aa984eac73ad157
Okiri Gabriel <66221309+OkiriGabriel@users.noreply.github.com>
2024-07-13 13:12:09 +0100

    Update userData.sh

--- userData.sh
+++ userData.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+sudo su
 yum update -y
 yum install httpd -y 
-echo "<html><h1>webpage 1(whatever you want, give the page name here)</h1></html>" > /var/www/html/index.html
+echo "<html><h1>Welcome, this is my host: $(hostname) Server!</h1></html>" > /var/www/html/index.html
 service httpd start
 chkconfig httpd on
